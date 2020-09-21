8f34e94d519ce7dde36f30c52333989a7b35e700
Claro Alvino <c.alvino583@gmail.com>
2020-09-20 22:31:45 -0300

    Update 1 questao letra b4.sql
    corrigindo a questao

--- semana 04/1 questao letra b4.sql
+++ semana 04/1 questao letra b4.sql
@@ -1,4 +1,3 @@
-use biblioteca;
-select titulo
-from livro
-where cod_livro = (select cod_livro from livro_emprestimo where data_emprestimo between '2020-08-01' AND '2020-09-01');
+select livro.titulo
+ from livro
+	inner join livro_emprestimo on (livro.cod_livro = livro_emprestimo.cod_livro and livro_emprestimo.data_emprestimo between '2020-08-01' and '2020-09-01');
